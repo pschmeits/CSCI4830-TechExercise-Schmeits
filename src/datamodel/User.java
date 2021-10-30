845a447cf63a27f79261ec30d2db58bb11ae8ff0
bye0n <parkerschmeits@gmail.com>
2021-10-29 23:55:53 -0500

    Added Note Storing Functionality to User Session

--- src/datamodel/User.java
+++ src/datamodel/User.java
@@ -1,4 +1,5 @@
 package datamodel;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
--- src/util/UtilDB.java
+++ src/util/UtilDB.java
@@ -2,22 +2,20 @@
  */
 package util;
 
-import java.util.List;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Iterator;
-
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
-import org.hibernate.service.ServiceRegistryBuilder;
-
-import datamodel.User;
+import java.util.List;
 
 import org.hibernate.HibernateException;
-import org.hibernate.Query;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+import datamodel.User;
+import datamodel.Note;
 
 /**
  * @since JavaSE-1.8
@@ -130,4 +128,73 @@ public class UtilDB {
          session.close();
       }
    }
+   
+   public static List<Note> listNotes(String userName) {
+      List<Note> resultList = new ArrayList<Note>();
+
+      Session session = getSessionFactory().openSession();
+      Transaction tx = null;
+
+      try {
+         tx = session.beginTransaction();
+         // System.out.println((User)session.get(User.class, 1)); // use "get" to fetch data
+         // Query q = session.createQuery("FROM User");
+         List<?> notes = session.createQuery("FROM Note").list();
+         for (Iterator<?> iterator = notes.iterator(); iterator.hasNext();) {
+            Note note = (Note) iterator.next();
+            if (note.getOwnerID().equals(userName)) {
+               resultList.add(note);
+            }
+         }
+         tx.commit();
+      } catch (HibernateException e) {
+         if (tx != null)
+            tx.rollback();
+         e.printStackTrace();
+      } finally {
+         session.close();
+      }
+      return resultList;
+	}
+   
+	public static void createNote(String ownerID, String label, String note) {
+	   if (!(ownerID == null || label == null || note == null)) {
+		   Session session = getSessionFactory().openSession();
+		   Transaction tx = null;
+		   try {
+			   tx = session.beginTransaction();
+			   session.save(new Note(ownerID, label, note));
+			   tx.commit();
+		   } catch (HibernateException e) {
+			   if (tx != null)
+				   tx.rollback();
+			   e.printStackTrace();
+		   } finally {
+			   session.close();
+		   }
+	   }
+	}
+   
+   // Main code for method modified from CodeJava.net (https://www.codejava.net/frameworks/hibernate/hibernate-basics-3-ways-to-delete-an-entity-from-the-datastore)
+   public static void deleteNote(int noteID) {
+	   Session session = getSessionFactory().openSession();
+	   Transaction tx = null;
+	   try {
+		   tx = session.beginTransaction();
+		   Serializable id = noteID;
+		   Object persistentInstance = session.load(Note.class, id);
+		   if (persistentInstance != null) {
+			   System.out.println("Deleting... " + persistentInstance.toString());
+			   session.delete(persistentInstance);
+			   tx.commit();
+		   }
+	   } catch (HibernateException e) {
+		   if (tx != null)
+			   tx.rollback();
+		   e.printStackTrace();
+	   } finally {
+		   session.close();
+	   }
+	}
 }
+
